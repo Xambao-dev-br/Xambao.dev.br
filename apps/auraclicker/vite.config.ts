d05f2694ab583f359c2bbca62e376900fb586715
ItsDoomOne <leodroid796@gmail.com>
2026-03-27 09:19:48 -0300

    Cache!

--- apps/auraclicker/vite.config.ts
+++ apps/auraclicker/vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
+  plugins: [react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    }),
 ],
 })
