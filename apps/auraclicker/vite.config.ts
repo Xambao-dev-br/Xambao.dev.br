6799f4818840b9e1ec7dacb0acbab84eba351ca7
ItsDoomOne <leodroid796@gmail.com>
2026-03-27 09:26:09 -0300

    Removido o Pwa offline

--- apps/auraclicker/vite.config.ts
+++ apps/auraclicker/vite.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,11 +7,5 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), 
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      }
-    }),
 ],
 })
